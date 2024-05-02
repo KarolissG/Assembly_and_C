ad55080b48a6000c7b29fe9d093bdee69d1effdc
karolissg <karolis.griga@gmail.com>
2024-05-02 19:48:54 +0100

    ok

--- Project_II/adder.asm
+++ Project_II/adder.asm
@@ -20,8 +20,8 @@ _start:	                                 ;linker entry point
 ;TAKE INPUT
 	xor	rdi,	rdi			;file descriptor (sdtin)
 	xor	rax,	rax			;clear rax for input
-	mov rsi,	buffer
-	mov rdx,	0x64
+	mov rsi,	buffer		;buffer foir first input
+	mov rdx,	buffer_size	;buffer size 100
 	syscall
 
 ;PRINT PROMPT
@@ -37,19 +37,35 @@ _start:	                                 ;linker entry point
 ;TAKE INPUT
 	xor	rdi,	rdi			;file descriptor (sdtin)
 	xor	rax,	rax			;clear rax for input
-	mov rsi,	buffer
-	mov rdx,	0x64
+	mov rsi,	buffer2		;buffer forsecond input
+	mov rdx,	buffer_size
+	syscall
+
+	mov rdx,	[buffer1]
+	sub rdx,	'0'
+	mov rbx,	[buffer2]
+	sub rbx,	'0'
+	call Adder1
+
+	mov	rdx,	result_lenght
+	mov rsi,	result
+	mov	rdi,	0x1
+	mov rax,	0x1
 	syscall
 
 ; Exit
     mov	rax,    0x3c                       ;system call number (sys_exit) 64 Bit Register
 
-    mov   rdi,    0x0                        ;return status 64 Bit Register
+    mov	rdi,    0x0                        ;return status 64 Bit Register
 
     syscall                                ;call kernel 64 bit System
 
 ; add two numbers
 Adder1:
+	add	rdx,	rbx
+	add	rdx,	'0'
+	mov [result],	rdx
+	ret
 
 
 ; DATA SECTION
@@ -60,3 +76,7 @@ msg_length equ $-message                  ;length of the string
 sum_msg db 'The sum is :',
 sum_lenght equ $-sum_msg
 buffer db 100
+buffer2	db 100
+buffer_size db 100
+result db 100
+result_lenght equ $-result
