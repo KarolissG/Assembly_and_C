6172cd1448380bd96821bdc9d202e3ae9abeabea
karolissg <karolis.griga@gmail.com>
2024-05-03 00:24:52 +0100

    saj

--- Project_II/adder.asm
+++ Project_II/adder.asm
@@ -1,77 +1,58 @@
-
 ; GLOBALS
 global _start ;Declared for linker this is declaring _start (entry point)
-
 
 ; TEXT SECTION
 section	.text
 _start:	                                 ;linker entry point
 
-
 ;PRINT PROMPT
-	mov	rdx,	msg_length                 ;message length see length equ 64 Bit Register
-
-	mov	rsi,	message                    ;message 64 Bit Register
-
-	mov	rdi,	0x1                        ;file descriptor (stdout) 64 Bit Register
-
-    mov	rax,  	0x1                        ;system call number (sys_write) 64 Bit Register
-	syscall                                ;call kernel 64 bit System
-
+	call PROMPT
 ;TAKE INPUT
-	xor	rdi,	rdi			;file descriptor (sdtin)
+	xor	rbx,	rbx			;file descriptor (sdtin)
 	xor	rax,	rax			;clear rax for input
-	mov rsi,	buffer		;buffer foir first input
-	mov rdx,	buffer_size	;buffer size 100
+	mov	rsi,	num1		;buffer foir first input
 	syscall
 
 ;PRINT PROMPT
-	mov	rdx,	msg_length                 ;message length see length equ 64 Bit Register
-
-	mov	rsi,	message                    ;message 64 Bit Register
-
-	mov	rdi,	0x1                        ;file descriptor (stdout) 64 Bit Register
-
-    mov	rax,  	0x1                        ;system call number (sys_write) 64 Bit Register
-    syscall
-
+	call PROMPT
 ;TAKE INPUT
-	xor	rdi,	rdi			;file descriptor (sdtin)
-	xor	rax,	rax			;clear rax for input
-	mov rsi,	buffer2		;buffer for second input
-	mov rdx,	buffer_size
+	xor	rbx,	rbx			;file descriptor (sdtin)
+	xor	rax,	rax			;clear rax for i
+	mov	rsi,	num2
 	syscall
 
-	mov	rdx,	0x1
-	mov	rsi,	buffer
-	mov	rdi,	0x1
-	mov	rax,	0x1
-	syscall
-
-;convert to integer
-	mov rdx,	buffer
-	sub rdx,	'0'
-	mov rbx,	buffer2
-	sub rbx,	'0'
 	call Adder1
-	mov	[result],	rdx
 
-	mov	rdx,	result_lenght
-	mov rsi,	result
-	mov	rdi,	0x1
-	mov rax,	0x1
+	mov	rdx,	64
+	mov	rsi,	num1
+	mov	rbx,	0x01
+	mov	rax,	0x01
+	syscall
+;NEWLINE
+	mov	rax,	1
+	mov	rdi,	1
+	mov	rsi,	newline
+	mov	rdx,	1
 	syscall
 
 ; Exit
-    mov	rax,    0x3c                       ;system call number (sys_exit) 64 Bit Register
+	mov	rax,    0x3c                       ;system call number (sys_exit) 64 Bit Register
+	mov	rdi,    0x0                        ;return status 64 Bit Register
+	syscall                               ;call kernel 64 bit System
 
-    mov	rdi,    0x0                        ;return status 64 Bit Register
+PROMPT:
+	mov	rdx,	msg_length                 ;message length see length equ 64 Bit Register
 
-    syscall                                ;call kernel 64 bit System
+	mov	rsi,	message                    ;message 64 Bit Register
 
+	mov	rbx,	0x01                       ;file descriptor (stdout) 64 Bit Register
+
+	mov	rax,  	0x01                        ;system call number (sys_write) 64 Bit Register
+	syscall                                ;call kernel 64 bit System
+	ret
 ; add two numbers
 Adder1:
-	add	rdx,	rbx
+	add	num1,	num2
 	ret
 
 
@@ -82,7 +63,12 @@ message db 'Enter Number :',    ;string to be printed
 msg_length equ $-message                  ;length of the string
 sum_msg db 'The sum is :',
 sum_lenght equ $-sum_msg
-buffer db 64
-buffer2	db 64
-buffer_size db 64
+buffer db 16
+buffer2	db 16
+buffer_size db 16
 result db 0
+newline db 10
+
+section .bss
+	num1	resb	64
+	num2	resb	64
